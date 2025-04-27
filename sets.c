5b2cf90095be07c01077dba728e93cc26354e41b
Miles Roberts <milesrobertsmain@gmail.com>
2025-04-27 17:16:40 -0500

    more concrete direction

--- sets.c
+++ sets.c
@@ -1,50 +1,55 @@
-#include <stdbool.h>
 #include <stdlib.h>
-
-/*
-// a library for creating sets in a modular system
-// stores truth value of [0,mod)
-
-// initialize an empty set
-bool* initialize_set(int mod) {
-    bool set[mod];
-    return set;
-}
-*/
-
-
 
 // a library for working with sets of integers
 // it is a dynamically sized array, with elements kept in order
 // to allow for a binary search
 
-struct element {
-    struct node* next;
-    int val;
+struct set {
+    int* arr;
+    int size;
 };
 
-// adds value 'n' to the set
-void add_to_set(int n) {
-
-}
-
-// removes value 'n' to the set
-void remove_from_set(int n) {
-
-}
-
 // initializes an empty set
-struct node* initialize_set() {
-    struct element* head = malloc(sizeof(struct element));
-
+struct set* initialize_set() {
+    struct set* set = malloc(sizeof(struct set));
+    set -> arr = malloc(0);
+    set -> size = 0;
+    return set;
 }
 
 // frees set from memory
-void free_set(struct node* head) {
+void free_set(struct set* set) {
 
 }
 
-// checks if 'n' is disjoint with the set
-int is_disjoint(int n, struct node* head) {
+// adds value n to the set
+// if n is already in the set, nothing is done
+void add_to_set(struct set* set, int n) {
+    // if n is disjoint with set 
+    set -> size++;
+    set -> arr = realloc(set -> arr, set -> size * sizeof(int));
+    set -> arr[set -> size - 1] = n;
+}
 
+// removes value n to the set
+// if n is not in the set, nothing is done
+void remove_from_set(struct set* set, int n) {
+
+}
+
+// returns 1 if n is disjoint with the set, otherwise 0
+int is_disjoint(struct set* set, int n) {
+    return -1;
+}
+
+int main() {
+    #include <stdio.h>
+    struct set* set = initialize_set();
+
+    add_to_set(set,5);
+    add_to_set(set, 3);
+
+    printf("%d %d\n", set -> arr[0], set -> arr[1]);
+
+    return 0;
 }
