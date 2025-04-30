c9522f8315819f4a5e6feeecd093310ba19e1fb6
Miles Roberts <milesrobertsmain@gmail.com>
2025-04-30 12:27:57 -0500

    renamed functions

--- sets.c
+++ sets.c
@@ -19,7 +19,7 @@ struct set* initialize_set() {
 }
 
 // frees set from memory
-void delete_set(struct set* set) {
+void set_delete(struct set* set) {
     free(set -> arr);
     free(set);
 }
@@ -40,7 +40,7 @@ int index_of_n_in_arr(struct set* set, int n) {
 }
 
 // returns 1 if n is disjoint with the set, otherwise 0
-int is_disjoint_int(struct set* set, int n) {
+int set_is_disjoint(struct set* set, int n) {
     if(index_of_n_in_arr(set, n) == -1)
         return 1;
     else
@@ -48,16 +48,16 @@ int is_disjoint_int(struct set* set, int n) {
 }
 
 // returns 1 if set1 is disjoint with set2, otherwise 0
-int is_disjoint_set(struct set* set1, struct set* set2) {
+int sets_are_disjoint(struct set* set1, struct set* set2) {
     for(int i = 0; i < set1 -> size; i++)
-        if(!is_disjoint_int(set2, set1 -> arr[i]))
+        if(!set_is_disjoint(set2, set1 -> arr[i]))
             return 0;
     return 1;
 }
 
 // adds value n to the set
-void add_to_set(struct set* set, int n) {
-    if(is_disjoint_int(set, n)) {
+void set_add(struct set* set, int n) {
+    if(set_is_disjoint(set, n)) {
         // increase size of set by 1
         set -> size++;
         set -> arr = realloc(set -> arr, set -> size * sizeof(int));
@@ -73,8 +73,8 @@ void add_to_set(struct set* set, int n) {
 }
 
 // removes value n to the set
-void remove_from_set(struct set* set, int n) {
-    if(!is_disjoint_int(set, n)) {
+void set_remove(struct set* set, int n) {
+    if(!set_is_disjoint(set, n)) {
         int* arr = set -> arr;
 
         for(int i = index_of_n_in_arr(set, n); i < set -> size - 1; i++)
@@ -85,40 +85,65 @@ void remove_from_set(struct set* set, int n) {
     }
 }
 
-void print_set(struct set* set) {
+void set_print(struct set* set) {
+    if(set -> size == 0)
+        printf("NULL");
     for(int i = 0; i < set -> size; i++) {
         printf("%d ", set -> arr[i]);
     }
     printf("\n");
 }
 
+// returns the union of two sets
+struct set* set_union(struct set* set1, struct set* set2) {
+    struct set* set3 = initialize_set();
+
+    for(int i = 0; i < set1 -> size; i++)  {
+            for(int j = 0; j < set2 -> size; j++) {
+                set_add(set3, set1 -> arr[i]);
+                set_add(set3, set2 -> arr[j]);
+        }
+    }
+    return set3;
+}
+
+// returns the intersection between two sets
+struct set* set_intersection(struct set* set1, struct set* set2) {
+    struct set* set3 = initialize_set();
+
+    for(int i = 0; i < set1 -> size; i++)
+        if(!set_is_disjoint(set2, set1 -> arr[i])) {
+            printf("%d %d\n", set1 -> arr[i]);
+            set_add(set3, set1 -> arr[i]);
+        }
+    
+    return set3;
+}
+
 int main() {
     struct set* set = initialize_set();
     struct set* set1 = initialize_set();
     
-    add_to_set(set, 1);
-    add_to_set(set1,0);
-    add_to_set(set, 3);
-    add_to_set(set1, 5);
-    add_to_set(set, 2);
-    add_to_set(set1, 4);
+    set_add(set, 1);
+    set_add(set1,0);
+    set_add(set, 3);
+    set_add(set1, 5);
+    set_add(set, 2);
+    set_add(set1, 4);
 
-    print_set(set);
-    print_set(set1);
+    set_remove(set, 2);
+    set_remove(set1, 4);
 
-    printf("%d\n", index_of_n_in_arr(set, 0));
+    set_print(set);
+    set_print(set1);
 
-    remove_from_set(set, 2);
-    remove_from_set(set1, 4);
+    struct set* nullset = initialize_set();
 
-    print_set(set);
-    print_set(set1);
+    struct set* set2 = set_union(set, set1);
+    set_print(set2);
 
-    printf("%d\n", is_disjoint_set(set, set1));
-
-    add_to_set(set1, 1);
-
-    printf("%d\n", is_disjoint_set(set, set1));
+    struct set* set3 = set_intersection(set, set1);
+    set_print(set3);
 
     return 0;
 }
